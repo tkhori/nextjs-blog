87ecac2bba9613d5340300e70c7253423e64ea4b
takuma hori <takuma.hr@gmail.com>
2021-03-07 17:36:03 +0900

    Render markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,7 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -21,11 +21,13 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      title: {postData.title}
+      {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
